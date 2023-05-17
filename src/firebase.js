1849da2a1a4144cc399a31c2ee0a8c86e838a302
Paulo Roberto <paulocarlos2468@gmail.com>
2023-05-17 10:36:49 -0300

    adicionando autenticação do firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -27,8 +28,8 @@ function adicionarUsuario(nome, email, idade, mensagem) {
   db.collection("users").add({
     nome: nome,
     email: email,
-    idade: idade,
-    mensagem: mensagem
+    senha: senha, 
+    confsenha: confsenha
   })
   .then(function(docRef) {
     console.log("Usuário adicionado com ID: ", docRef.id);
@@ -41,18 +42,119 @@ function adicionarUsuario(nome, email, idade, mensagem) {
 // Use a função adicionarUsuario para adicionar um novo usuário ao banco de dados
 // Obtenha os valores do formulário HTML e passe-os para a função adicionarUsuario
 
-const form = document.querySelector("form");
+const form = document.querySelector("index");
 
 form.addEventListener("submit", function(event) {
   event.preventDefault(); // Evita o comportamento padrão de envio do formulário
 
   const nome = document.getElementById("nome").value;
   const email = document.getElementById("email").value;
-  const idade = document.getElementById("idade").value;
-  const mensagem = document.getElementById("mensagem").value;
+  const senha = document.getElementById("senha").value;
+  const confsenha = document.getElementById("confsenha").value;
 
-  adicionarUsuario(nome, email, idade, mensagem);
+  adicionarUsuario(nome, email, senha);
 
   // Limpe os campos do formulário após o envio
   form.reset();
 });
+
+// Criar uma conta de usuário com email/senha
+auth.createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Registro bem-sucedido, você pode acessar o usuário criado através de userCredential.user
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    // Lidar com erros durante o registro
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+// Fazer login com email/senha
+auth.signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Login bem-sucedido, você pode acessar o usuário logado através de userCredential.user
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    // Lidar com erros durante o login
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+// Fazer logout
+auth.signOut()
+  .then(() => {
+    // Logout bem-sucedido
+  })
+  .catch((error) => {
+    // Lidar com erros durante o logout
+  });
+
+// Observar o estado de autenticação do usuário
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // O usuário está logado, você pode acessar o objeto user
+  } else {
+    // O usuário não está logado
+  }
+});
+
+/**
+ * // Importe o SDK do Firebase
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Configure a configuração do Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO",
+  projectId: "SEU_ID_DO_PROJETO",
+  // ...
+};
+
+// Inicialize o app do Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Obtenha referências para o Firestore e para o módulo de autenticação
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+// Função para adicionar usuário ao Firestore
+function adicionarUsuario(nome, email, senha) {
+  return db.collection("users").add({
+    nome: nome,
+    email: email,
+    senha: senha
+  });
+}
+
+// Função para criar uma conta de usuário com email/senha
+function criarContaUsuario(email, senha) {
+  return auth.createUserWithEmailAndPassword(email, senha);
+}
+
+// Função para fazer login com email/senha
+function fazerLogin(email, senha) {
+  return auth.signInWithEmailAndPassword(email, senha);
+}
+
+// Exemplo de uso das funções
+adicionarUsuario("John Doe", "johndoe@example.com", "senha123")
+  .then((docRef) => {
+    console.log("Usuário adicionado com ID: ", docRef.id);
+    return criarContaUsuario("johndoe@example.com", "senha123");
+  })
+  .then((userCredential) => {
+    console.log("Conta criada com sucesso para o usuário: ", userCredential.user.email);
+    return fazerLogin("johndoe@example.com", "senha123");
+  })
+  .then((userCredential) => {
+    console.log("Usuário logado com sucesso: ", userCredential.user.email);
+  })
+  .catch((error) => {
+    console.error("Erro: ", error);
+  });
+
+ */
